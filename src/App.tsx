cb354dbd322a5a0e15020538e7a2b60bf2628574
Letícia Diniz <leticiacalixtodiniz@gmail.com>
2025-03-21 10:26:10 -0300

    Adicionando LoginForm

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Cabecalho from './componets/Cabecalho/Cabecalho'
 import Rodape from './componets/Rodape/Rodape'
 import Wellcome from './componets/Wellcome/Wellcome'
+import LoginForm from './componets/LoginForm/LoginForm'
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
     <Cabecalho/>
-    <Wellcome/>
+    <LoginForm/>
     <Rodape/>
     </>
   )
--- src/componets/Cabecalho/Cabecalho.tsx
+++ src/componets/Cabecalho/Cabecalho.tsx
@@ -1,9 +1,10 @@
 import estilo from './Cabecalho.module.css'
+import logotipo from '../../assets/logotipo.png'
 
 function Cabecalho (){
     return(
-        <header className= {estilo.Cabecalho}>
-            <h1>Logotipo</h1>
+        <header className= {estilo.cabecalho}>
+            <img src={logotipo} alt="logotipo" />
             <a href="#">Login</a>
         </header>
     )
